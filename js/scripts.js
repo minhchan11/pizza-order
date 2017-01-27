6b916682e4f6e3f771cc34c1a56ebba19c52661c
Minh Phuong <mphuong@kent.edu>
2017-01-27 11:27:08 -0800

    add Business Logic add global variable total add constructor pizza

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//BUSINESS LOGIC
+var total = 0;
+function pizza(pizzaSize,crust,toppings,sauce,quantity) {
+  this.pizzaSize = pizzaSize;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.sauce = sauce;
+  this.quantity = quantity;
+}
